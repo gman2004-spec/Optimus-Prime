a4be33c45c323ac623758b7e6c03f00271602e38
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-04-30 16:48:57 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <h1 className="text-red-600">Pedro</h1>
+      <img src="./Assets/MasterChief.webp" alt="MasterChief" height="100px" width="100px" />
+      
+    </div>
+  )
+}
