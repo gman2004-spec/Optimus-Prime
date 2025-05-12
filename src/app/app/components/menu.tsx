7b2f29e8ab81b71790528ebe8a05fb2194ef1ab9
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-05-12 14:24:06 -0300

    Update menu.tsx

--- src/app/app/components/menu.tsx
+++ src/app/app/components/menu.tsx
@@ -3,18 +3,14 @@ import Link from "next/link";
 interface MenuProps {
     op1: string;
     op2: string;
-    op3?: string;
-    op4?: string;
 }
 
-export const Menu: React.FC = ({op1, op2, op3, op4}) => {
+export const Menu = ({op1, op2} : MenuProps) => {
     return(
         <header>
             <nav className="flex">
-                <Link className="font-black" href={"/"}>{op1}</Link>
+                <Link className="font-black" href={"/serverSide"}>{op1}</Link>
                 <Link className="font-black" href={"/hookPage"}>{op2}</Link>
-                {/* <Link className="font-black" href={}></Link>
-                <Link className="font-black" href={}></Link> */}
             </nav>
         </header>
     )
